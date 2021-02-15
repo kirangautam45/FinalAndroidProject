786e8ccb11ee865598eca9234d4985ad7e4bd527
kirangautam45 <gautamkiran38@gmail.com>
2021-02-15 21:44:41 +0545

    login design created

--- app/src/main/java/com/example/finalproject/loginActivity.kt
+++ app/src/main/java/com/example/finalproject/loginActivity.kt
@@ -3,10 +3,9 @@ package com.example.finalproject
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class SplashActivity : AppCompatActivity() {
-
+class loginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
