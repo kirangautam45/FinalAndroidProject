c8003de88c3e86135dc30d6ce46e02d48413aaea
kirangautam45 <gautamkiran38@gmail.com>
2021-02-15 22:26:39 +0545

    splash complete0d

--- app/src/main/java/com/example/finalproject/RegistrationActivity.kt
+++ app/src/main/java/com/example/finalproject/RegistrationActivity.kt
@@ -2,10 +2,65 @@ package com.example.finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.finalproject.db.UserDB
+import com.example.finalproject.entity.User
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class RegistrationActivity : AppCompatActivity() {
+    private lateinit var Fname : EditText
+    private lateinit var Lname: EditText
+    private lateinit var Username: EditText
+    private lateinit var Address: EditText
+    private lateinit var Phone: EditText
+    private lateinit var Password: EditText
+    private lateinit var ConfirmPassword: EditText
+    private lateinit var btnAdduser: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+        Fname=findViewById(R.id.Fname)
+        Lname=findViewById(R.id.Lname)
+        Username=findViewById(R.id.Username)
+        Address=findViewById(R.id.Address)
+        Phone=findViewById(R.id.Phone)
+        Password=findViewById(R.id.Password)
+        ConfirmPassword=findViewById(R.id.ConfirmPassword)
+        btnAdduser=findViewById(R.id.btnAdduser)
+
+        btnAdduser.setOnClickListener {
+            val fname= Fname.text.toString()
+            val lname=Lname.text.toString()
+            val address = Address.text.toString()
+            val phone = Phone.text.toString()
+            val username = Username.text.toString()
+            val password =Password.text.toString()
+            val confirmpassword = ConfirmPassword.text.toString()
+
+            if(password != confirmpassword){
+                Password.error= "password does not match"
+                Password.requestFocus()
+                return@setOnClickListener }
+            else {
+                val user= User(fname,lname,address,phone,username,password)
+                CoroutineScope(Dispatchers.IO).launch { UserDB
+                    .getInstance(this@RegistrationActivity)
+                    .getUserDAO()
+                    .registerUser(user )
+                    //switch to main thread
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@RegistrationActivity,
+                            "user saved", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/finalproject/SplashActivity.kt
+++ app/src/main/java/com/example/finalproject/SplashActivity.kt
@@ -1,12 +1,57 @@
 package com.example.finalproject
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+    private val permission = arrayOf(
+        android.Manifest.permission.CAMERA,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        android.Manifest.permission.ACCESS_FINE_LOCATION
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (!hasPermission()) {
+            requestPermission()
+        }
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1000)
+
+            startActivity(
+                Intent(this@SplashActivity,
+                    loginActivity::class.java
+                )
+            )
+            finish()
+        }
+
+    }
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this@SplashActivity,
+            permission, 1
+        )
+    }
+    private fun hasPermission(): Boolean {
+        var hasPermission = true
+        for (permission in permission) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                hasPermission = false
+            }
+        }
+        return hasPermission
     }
 }
