c15cde61b329eb5b7d79874fe45102e32f7d6fac
kirangautam45 <gautamkiran38@gmail.com>
2021-02-15 22:20:33 +0545

    registration form created

--- app/src/main/java/com/example/finalproject/RegistrationActivity.kt
+++ app/src/main/java/com/example/finalproject/RegistrationActivity.kt
@@ -3,9 +3,9 @@ package com.example.finalproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class DashboardActivity : AppCompatActivity() {
+class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        setContentView(R.layout.activity_registration)
     }
 }
