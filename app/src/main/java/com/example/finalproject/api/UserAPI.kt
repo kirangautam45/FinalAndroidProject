e3d72ad0a121e86d4ffead1130d04a8a2cf8b6b8
Kiran Gautam <gautamkiran38@gmail.com>
2021-02-17 21:00:23 +0545

    response for user add

--- app/src/main/java/com/example/finalproject/api/UserAPI.kt
+++ app/src/main/java/com/example/finalproject/api/UserAPI.kt
@@ -1,5 +1,18 @@
 package com.example.finalproject.api
 
+import android.media.session.MediaSession
+import com.example.finalproject.entity.User
+import com.example.finalproject.respone.AddUserRespone
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+
 interface UserAPI {
-    @post("user/insert"):Respone<addUserRespone>
+    @POST("user/insert")
+    suspend fun addstudent(
+            @Header("Authorization") token: String,
+            @Body user: User
+
+    ):Response<AddUserRespone>
 }
