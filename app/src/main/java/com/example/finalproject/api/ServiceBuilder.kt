a40e63a5f68dff6d687e22414fe1dd5d3bb948af
Kiran Gautam <gautamkiran38@gmail.com>
2021-02-17 20:41:58 +0545

    service builder created

---
+++ app/src/main/java/com/example/finalproject/api/ServiceBuilder.kt
@@ -0,0 +1,17 @@
+package com.example.finalproject.api
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ServiceBuilder {
+    private  const val BASE_URL ="http://10.0.2.2:90"
+    var token:String?=null
+    private val okhttp =     OkHttpClient.Builder()
+
+    //creating retrofit builder
+    private val retrofitBuilder = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okhttp.build())
+}
