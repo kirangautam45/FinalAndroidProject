7acf19c5c1c6399a2b8bcd63cfc5ac4f0c2163d1
kirangautam45 <gautamkiran38@gmail.com>
2021-03-06 11:36:43 +0545

    luncher change

--- app/src/main/java/com/example/finalproject/SplashActivity.kt
+++ app/src/main/java/com/example/finalproject/SplashActivity.kt
@@ -1,8 +1,11 @@
 package com.example.finalproject
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -13,6 +16,17 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else{
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN )
+        }
+
+
 
         CoroutineScope(Dispatchers.Main).launch {
             delay(3000)
@@ -25,5 +39,7 @@ class SplashActivity : AppCompatActivity() {
             finish()
         }
 
+
     }
+
 }
