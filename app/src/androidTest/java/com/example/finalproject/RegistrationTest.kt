c3c9b9681fe9844cbc9cabedd42dee91fe3d20b7
kirangautam45 <gautamkiran38@gmail.com>
2021-03-10 20:23:03 +0545

    login test

---
+++ app/src/androidTest/java/com/example/finalproject/RegistrationTest.kt
@@ -0,0 +1,22 @@
+package com.example.finalproject
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.filters.LargeTest
+import com.example.finalproject.activity.RegistrationActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+class RegistrationTest {
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/finalproject/activity/BedroomActivity.kt
@@ -0,0 +1,12 @@
+package com.example.finalproject.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finalproject.R
+
+class BedroomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bedroom)
+    }
+}
