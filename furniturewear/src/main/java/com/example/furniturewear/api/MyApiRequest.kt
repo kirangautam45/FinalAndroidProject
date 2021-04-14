5d030e1f4b2e12a671d9b996b6af4b7724666aaf
kirangautam45 <gautamkiran38@gmail.com>
2021-04-14 20:48:59 +0545

    api connection for wear os

---
+++ furniturewear/src/main/java/com/example/furniturewear/api/MyApiRequest.kt
@@ -0,0 +1,27 @@
+package com.example.furniturewear.api
+
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+
+class MyApiRequest {
+    suspend fun <T:Any>apiRequest(call: suspend () -> Response<T>): T {
+        val response = call.invoke()
+        if(response.isSuccessful){
+            return response.body()!!
+        } else{
+            val error =response.errorBody()?.string()
+            val message= StringBuilder()
+            error?.let{
+                try {
+                    message.append(JSONObject(it).getString("success"))
+                } catch (e: JSONException){
+
+                }
+                message.append("\n")
+            }
+            message.append("Error code:${response.code()}")
+            throw IOException(message.toString())
+        }
+    }
+}
