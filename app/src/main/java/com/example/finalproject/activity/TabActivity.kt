9e70fb8ce93bce1f9fb188834df32eda69e149a0
kirangautam45 <gautamkiran38@gmail.com>
2021-04-17 16:30:57 +0545

    proximity sensor added

--- app/src/main/java/com/example/finalproject/activity/TabActivity.kt
+++ app/src/main/java/com/example/finalproject/activity/TabActivity.kt
@@ -1,6 +1,13 @@
 package com.example.finalproject.activity
 
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
@@ -13,8 +20,9 @@ import com.example.finalproject.fragments.DashboardActivity
 import com.example.finalproject.fragments.ProductAddActivity
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import java.io.IOException
 
-class TabActivity : AppCompatActivity() {
+class TabActivity : AppCompatActivity(),SensorEventListener {
     private val permissions = arrayOf(
         android.Manifest.permission.CAMERA,
         android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -24,10 +32,17 @@ class TabActivity : AppCompatActivity() {
     private lateinit var lstFragments:ArrayList<Fragment>
     private lateinit var viewpager:ViewPager2
     private lateinit var tabLayout: TabLayout
+    private var sensorManager: SensorManager?=null
+    private var sensor: Sensor?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tab)
+
+        sensorManager=getSystemService(Context.SENSOR_SERVICE)as SensorManager
+        sensor=sensorManager!!.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+
+
 
         if (!hasPermission()){
             requestPermission()
@@ -74,5 +89,34 @@ class TabActivity : AppCompatActivity() {
         }
         return hasPermission
 
+    }
+    override fun onResume() {
+        super.onResume()
+        sensorManager!!.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL)
+    }
+    override fun onPause() {
+        super.onPause()
+        sensorManager!!.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        val values = event!!.values[0]
+        if (values <= 4) {
+            try {
+                logoutapp()
+            } catch (e: IOException) {
+            }
+        }
+    }
+
+    private fun logoutapp() {
+        val sharedpref =getSharedPreferences("mypref", AppCompatActivity.MODE_PRIVATE)
+        val editor = sharedpref.edit()
+        editor.clear()
+        editor.apply()
+        startActivity(Intent(this@TabActivity,LoginActivity::class.java))
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
     }
 }
