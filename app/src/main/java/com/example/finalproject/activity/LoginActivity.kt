65ba5a01994bb72a4e9c5385cfb137e0e7d62d41
kirangautam45 <gautamkiran38@gmail.com>
2021-04-17 16:22:52 +0545

    light sensor added

--- app/src/main/java/com/example/finalproject/activity/LoginActivity.kt
+++ app/src/main/java/com/example/finalproject/activity/LoginActivity.kt
@@ -2,21 +2,13 @@ package com.example.finalproject.activity
 
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Color
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import com.example.finalproject.R
 import com.example.finalproject.api.ServiceBuilder
-import com.example.finalproject.notification.NotificationChannels
 import com.example.finalproject.repository.UserRepository
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_login.*
@@ -26,7 +18,6 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class LoginActivity : AppCompatActivity() {
-
     private val permissions = arrayOf(
             android.Manifest.permission.CAMERA,
             android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -43,8 +34,6 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-
         phone = findViewById(R.id.Phone)
         password = findViewById(R.id.Password)
         btnlogin = findViewById(R.id.btnLogin)
@@ -55,7 +44,6 @@ class LoginActivity : AppCompatActivity() {
 
         btnlogin.setOnClickListener {
             login()
-            showHighPriorityNotification()
         }
         register.setOnClickListener {
 
@@ -67,25 +55,6 @@ class LoginActivity : AppCompatActivity() {
                     )
             )
         }
-
-    }
-
-
-    private fun showHighPriorityNotification() {
-
-        val notificationManager = NotificationManagerCompat.from(this)
-
-        val notificationChannels = NotificationChannels(this)
-        notificationChannels.createNotificationChannels()
-
-        val notification = NotificationCompat.Builder(this, notificationChannels.CHANNEL_1)
-                .setSmallIcon(R.drawable.notification)
-                .setContentTitle("High priority notification")
-                .setContentText("user has login")
-                .setColor(Color.BLUE)
-                .build()
-
-        notificationManager.notify(1, notification)
 
     }
 
